7f893d4aaaeaa4e79feebfc278aea7c361d6bcfc
mariusz.matkowski <mariusz.matkowski@yahoo.com>
2025-05-25 14:38:18 +0200

    arm-none-eabi-gcc.cmake

---
+++ arm-none-eabi-gcc.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(GCC_VERSION 13.3)
+
+set(CMAKE_C_COMPILER /home/mariusz/opt/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER /home/mariusz/opt/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++)
+set(CMAKE_AR /home/mariusz/opt/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-ar)
+set(CMAKE_RANLIB /home/mariusz/opt/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-ranlib)
+set(CMAKE_OBJCOPY /home/mariusz/opt/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy)
+set(CMAKE_ASM_COMPILER /home/mariusz/opt/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc)
+
